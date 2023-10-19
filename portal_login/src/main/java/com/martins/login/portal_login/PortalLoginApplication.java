dc34b5a905caaa4bae337c2d42875d56cc901667
Diogo-M-a-r-t-i-n-s <engenheirodiogomartins@gmail.com>
2023-10-18 20:52:09 -0400

    inicio start.spring.io

---
+++ portal_login/src/main/java/com/martins/login/portal_login/PortalLoginApplication.java
@@ -0,0 +1,13 @@
+package com.martins.login.portal_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalLoginApplication.class, args);
+	}
+
+}
---
+++ portal_login/src/test/java/com/martins/login/portal_login/PortalLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martins.login.portal_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
